68e0181ebef19ccd67a0912d97664b59f29c0228
bae080311 <s24024@gsm.hs.kr>
2024-10-07 23:20:27 +0900

    feat: redirection

--- src/pages/starting.tsx
+++ src/pages/starting.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { Content } from "../components/content";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 
 export function Starting() {
+  const navigator = useNavigate();
   return (
-    <div className="flex flex-col ">
+    <div
+      className="flex flex-col"
+      onClick={() =>
+        setTimeout(() => {
+          navigator("/");
+        }, 5000)
+      }
+    >
       <Header />
       <Content />
       <Footer />
