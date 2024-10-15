52c76f239ae65948ddf87ba7c2680a3b132b9f47
bae080311 <s24024@gsm.hs.kr>
2024-10-15 12:19:40 +0900

    fix: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Starting } from "./pages/starting";
-import { Main } from "./pages/main";
+import { Options } from "./pages/options";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/start" element={<Starting />} />
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Starting />} />
+        <Route path="/options" element={<Options />} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
