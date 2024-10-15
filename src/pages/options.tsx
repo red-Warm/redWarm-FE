39e65f2e4a8a000081141251fced9c4ea0b4f51c
bae080311 <s24024@gsm.hs.kr>
2024-10-15 14:07:58 +0900

    fix: 글자 이미지와 겹치게 하기

--- src/pages/options.tsx
+++ src/pages/options.tsx
@@ -8,21 +8,27 @@ export function Options() {
     <div>
       <Header />
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-pointRed m-9">
+        <h1 className="mb-20 text-4xl font-bold text-pointRed m-9">
           보고싶은 전공을 클릭하세요.
         </h1>
-        <div className="flex gap-20">
-          <div>
-            <h3>공기업</h3>
+        <div className="flex justify-between gap-20">
+          <div className="relative">
             <img src={enterprise} className="w-[315px]" alt="공기업" />
+            <h3 className="absolute inset-0 flex items-center justify-center font-bold text-white">
+              공기업
+            </h3>
           </div>
-          <div>
-            <h3>개발자</h3>
+          <div className="relative">
             <img src={develop} className="w-[315px]" alt="개발자" />
+            <h3 className="absolute inset-0 flex items-center justify-center font-bold text-white">
+              개발자
+            </h3>
           </div>
-          <div>
-            <h3>디자이너</h3>
+          <div className="relative">
             <img src={design} className="w-[315px]" alt="디자이너" />
+            <h3 className="absolute inset-0 flex items-center justify-center font-bold text-white">
+              디자이너
+            </h3>
           </div>
         </div>
       </div>
