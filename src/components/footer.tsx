7c204dccaeb784a190d99ead418e34de880d55a7
bae080311 <s24024@gsm.hs.kr>
2024-10-15 14:20:46 +0900

    fix: 문구

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,14 +4,13 @@ export function Footer() {
       <div>
         <h1 className="my-3 text-2xl font-bold">GSM. Job</h1>
         <span>
-          자신에게 맞는 일을 하며 <br /> 프로젝트의 효율을 높여보아요!!
+          우리 학교의 취업률을 <br /> 그래프로 한눈에 볼 수 있습니다!
         </span>
       </div>
       <div className="text-right">
-        <h1 className="my-3 text-2xl font-bold">모두가 원하는 프로젝트</h1>
+        <h1 className="my-3 text-2xl font-bold">모두의 취업</h1>
         <span>
-          원하는 프로젝트를 쉽고 빠르게 선점하여 <br /> 가고 싶은 진로에 한
-          발자국 더 다가가요
+          우리 학교의 취업률을 <br />한 눈에 보세요!!
         </span>
       </div>
     </footer>
