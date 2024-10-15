5a1d4bb71bd44f6e9d7e73dd64e3f02f81a9c7b8
bae080311 <s24024@gsm.hs.kr>
2024-10-15 13:56:28 +0900

    feat: option 요소들

---
+++ src/pages/options.tsx
@@ -0,0 +1,29 @@
+import { Header } from "../components/header";
+import design from "../imgs/design.png";
+import develop from "../imgs/develop.png";
+import enterprise from "../imgs/enterprise.png";
+
+export function Options() {
+  return (
+    <div>
+      <Header />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-pointRed m-9">
+          보고싶은 전공을 클릭하세요.
+        </h1>
+        <div className="flex justify-between ">
+          <div>
+            <h3>공기업</h3>
+            <img src={enterprise} className="w-[315px]" alt="공기업" />
+          </div>
+          <div>
+            <h3>개발자</h3>
+            <img src={develop} className="w-[315px]" alt="개발자" />
+          </div>
+          <h3>디자이너</h3>
+          <img src={design} className="w-[315px]" alt="디자이너" />
+        </div>
+      </div>
+    </div>
+  );
+}
