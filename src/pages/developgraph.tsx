f3288a05d013c562f7a4823acebba49a52d64b3f
bae080311 <s24024@gsm.hs.kr>
2024-10-18 00:41:31 +0900

    🔥delete border

--- src/pages/developgraph.tsx
+++ src/pages/developgraph.tsx
@@ -16,7 +16,7 @@ Chart.register(CategoryScale, LinearScale, BarElement, BarController, Title);
 export function DevelopGraph() {
   const [data, setData] = useState<developergraph>({
     frontEnd: 30,
-    backEnd: 60,
+    backEnd: 62,
     Android: 10,
     IOS: 20,
   });
@@ -29,7 +29,7 @@ export function DevelopGraph() {
         })
         .catch((err) => {
           console.error("API 요청 오류:", err);
-          alert("그래프르 불러오는데 실패했습니다.");
+          alert("그래프를 불러오는데 실패했습니다.");
         });
     }
 
@@ -43,12 +43,11 @@ export function DevelopGraph() {
       const graph = new Chart(chartRef.current, {
         type: "bar",
         data: {
-          labels: ["frontEnd", "backEnd", "Android", "IOS"],
+          labels: ["FrontEnd", "BackEnd", "Android", "IOS"],
           datasets: [
             {
               label: "개발자",
               data: [data.frontEnd, data.backEnd, data.Android, data.IOS],
-              borderWidth: 1,
               backgroundColor: ["blue", "red", "green", "yellow"],
             },
           ],
@@ -74,7 +73,7 @@ export function DevelopGraph() {
     <div>
       <Header />
       <div>
-        <canvas className="h-[650px]" ref={chartRef}></canvas>
+        <canvas className="font-bold h-[650px]" ref={chartRef}></canvas>
       </div>
     </div>
   );
